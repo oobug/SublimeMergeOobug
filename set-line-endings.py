1e32e9eaf6bb00184c1a0dd1fa4db9ad41ae3d68
Curtis den Otter <curtis.denotter@computronix.com>
2019-12-23 14:11:14 -0700

    Add Diff Context Menu item to set line endings

---
+++ set-line-endings.py
@@ -0,0 +1,36 @@
+"""Set the line endings of a specified file.
+
+Arguments:
+    path-to-file, line-ending-type
+
+To Use, add the following to your .gitconfig, updating "path/to"
+appropriately:
+
+[alias]
+    set-line-endings = "!f() { /c/Python27/python.exe /c/path/to/set-line-endings.py $1 $2; }; f"
+"""
+
+import sys
+
+# replacement strings
+UNIX_NEWLINE = '\n'
+WINDOWS_NEWLINE = '\r\n'
+MAC_NEWLINE = '\r'
+
+# relative or absolute file path, e.g.:
+file_path = sys.argv[1]
+
+with open(file_path, 'rb') as open_file:
+    content = open_file.read()
+
+if sys.argv[2] == 'windows':
+    content = WINDOWS_NEWLINE.join(content.splitlines())
+else:
+    content = content.replace(
+        WINDOWS_NEWLINE, UNIX_NEWLINE
+    ).replace(
+        MAC_NEWLINE, UNIX_NEWLINE
+    )
+
+with open(file_path, 'wb') as open_file:
+    open_file.write(content)

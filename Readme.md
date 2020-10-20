605e8136c30517d0a6649799a98656d0eee0fb16
Curtis den Otter <curtis.denotter@computronix.com>
2020-10-20 12:06:08 -0600

    Use Python Launcher and APPDATA path in set-line-endings

--- Readme.md
+++ Readme.md
@@ -4,6 +4,10 @@ A repository to contain my settings, snippets, and extensions for Sublime Merge.
 
 ## Git Config settings
 
+### Set line endings
+    [alias]
+        set-line-endings = "!f() { py \"${APPDATA}/Sublime Merge/Packages/SublimeMergeOobug/set-line-endings.py\" $1 $2; }; f"
+
 ### TortoiseGit diff tools
 
 From https://github.com/TortoiseGit/TortoiseGit/tree/master/contrib/diff-scripts
--- set-line-endings.py
+++ set-line-endings.py
@@ -3,11 +3,10 @@
 Arguments:
     path-to-file, line-ending-type
 
-To Use, add the following to your .gitconfig, updating "path/to"
-appropriately:
+To use, add the following to your .gitconfig:
 
 [alias]
-    set-line-endings = "!f() { /c/Python27/python.exe /c/path/to/set-line-endings.py $1 $2; }; f"
+    set-line-endings = "!f() { py \"${APPDATA}/Sublime Merge/Packages/SublimeMergeOobug/set-line-endings.py\" $1 $2; }; f"
 """
 
 import sys
@@ -20,7 +19,7 @@ MAC_NEWLINE = '\r'
 # relative or absolute file path, e.g.:
 file_path = sys.argv[1]
 
-with open(file_path, 'rb') as open_file:
+with open(file_path, 'r') as open_file:
     content = open_file.read()
 
 if sys.argv[2] == 'windows':
@@ -32,5 +31,5 @@ else:
         MAC_NEWLINE, UNIX_NEWLINE
     )
 
-with open(file_path, 'wb') as open_file:
+with open(file_path, 'w') as open_file:
     open_file.write(content)

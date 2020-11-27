44e0e98b242825d3703bccc1be533280e7fa1896
Curtis den Otter <curtis.denotter@gmail.com>
2020-11-26 19:54:48 -0700

    Bitbucket integration (#1)
    * Add initial Bitbucket integration
    
    Source: https://forum.sublimetext.com/t/github-gitlab-bitbucket-integration-commands-menu-items/53893
    
    * Add initial Python version of integration
    
    * Write remainder of Python integration
    
    * Support Python versions < 3.6
    
    * Encapsulate script body in function
    
    * Add a docstring to the new main function
    
    * Rename file to allow importing
    
    * Include proper installation instructions
    
    * Open URL using os.system instead of webbrowser
    
    Running the alias from Sublime Merge does not work with webbrowser
    
    * Only remove ".git" suffix if present
    
    * Use Python script to open URL
    
    * Fix credit URL
    
    * Validate URL before passing to "explorer"
    
    Co-authored-by: Curtis den Otter <curtis.denotter@computronix.com>

---
+++ open_url.py
@@ -0,0 +1,29 @@
+"""open_url: Open a URL in the system default web browser
+
+Uses os.system instead of the webbrowser module to work around a
+limitation in Sublime Merge.
+"""
+
+import os
+import re
+import sys
+
+url = sys.argv[1]
+
+# Validate URL
+urlRegex = re.compile(
+    # http:// or https://
+    r"^https?://"
+    # domain...
+    r"(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|"
+    # localhost...
+    r"localhost|"
+    # ...or ip
+    r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})"
+    # optional port
+    r"(?::\d+)?" r"(?:/?|[/?]\S+)$",  # end of URL
+    re.IGNORECASE,
+)
+
+if urlRegex.match(url):
+    os.system('explorer "{}"'.format(url))
